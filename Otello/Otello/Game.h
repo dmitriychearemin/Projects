4212450ee0fb03be2af4d1669af354c77b068171
dmitriychearemin <126575364+dmitriychearemin@users.noreply.github.com>
2023-10-02 09:57:34 +0300

    Update Game.h

--- Otello/Otello/Game.h
+++ Otello/Otello/Game.h
@@ -3,16 +3,16 @@
 
 class Game {
 	private:
-		int Counts_Point_White=2, Counts_Point_Black=2;		// Количество фишек черного и белого цветов
-		int WIDTH_Screen=1000, Height_Screen=1000;          // Разрешение экрана
-		int** Game_Field;                                   // Игровое поле
-		sf::CircleShape* Tocken = new sf::CircleShape[64];  // Фишки
-
+		int Counts_Point_White=2, Counts_Point_Black=2;		// ГЉГ®Г«ГЁГ·ГҐГ±ГІГўГ® ГґГЁГёГҐГЄ Г·ГҐГ°Г­Г®ГЈГ® ГЁ ГЎГҐГ«Г®ГЈГ® Г¶ГўГҐГІГ®Гў
+		int WIDTH_Screen=1000, Height_Screen=1000;          // ГђГ Г§Г°ГҐГёГҐГ­ГЁГҐ ГЅГЄГ°Г Г­Г 
+		int** Game_Field;                                   // Г€ГЈГ°Г®ГўГ®ГҐ ГЇГ®Г«ГҐ
+		sf::CircleShape* Tocken = new sf::CircleShape[64];  // Г”ГЁГёГЄГЁ
+  int Koef = 150;
 
 	public:
 		void Game_Cycle();
 		void Create_Field();
 		void Update_Display();
-		void 
-
+		void Create_Tocken();
+  void Building_On_Array();
 };
