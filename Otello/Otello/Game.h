50c36b76efb58758fada554bdda3f368ae1748aa
Cheremin Dmitriy <126575364+dmitriychearemin@users.noreply.github.com>
2023-10-02 08:40:34 +0300

    Otello

--- Otello/Otello/Game.h
+++ Otello/Otello/Game.h
@@ -2,16 +2,17 @@
 #include <SFML/Graphics.hpp>
 
 class Game {
-
 	private:
-		int Counts_Point_White=0, Counts_Point_Black=0;
-		int WIDTH_Screen=1000, Height_Screen=1000;
-		sf::Texture Background_Texture;
+		int Counts_Point_White=2, Counts_Point_Black=2;		// Количество фишек черного и белого цветов
+		int WIDTH_Screen=1000, Height_Screen=1000;          // Разрешение экрана
+		int** Game_Field;                                   // Игровое поле
+		sf::CircleShape* Tocken = new sf::CircleShape[64];  // Фишки
+
 
 	public:
 		void Game_Cycle();
 		void Create_Field();
-
-
+		void Update_Display();
+		void 
 
 };
--- Otello/Otello/Game_Realization.cpp
+++ Otello/Otello/Game_Realization.cpp
@@ -6,6 +6,7 @@ void Game::Game_Cycle() {
 	sf::RenderWindow window(sf::VideoMode(WIDTH_Screen,Height_Screen), "Otello");
 	
 	sf::RectangleShape BackGr(sf::Vector2f(800, 800));
+	sf::Texture Background_Texture;
 	Background_Texture.loadFromFile("Background.PNG");
 	BackGr.setOrigin(400, 400);
 	BackGr.setPosition(WIDTH_Screen / 2, Height_Screen / 2);
@@ -29,7 +30,36 @@ void Game::Game_Cycle() {
 }
 
 void Game::Create_Field() {
+	
+	*Game_Field = new int[8];
+	for (int i = 0; i < 8; i++) {
+		Game_Field[i] = new int[8];
+	}
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if (i == 3 && j == 3) {
+				Game_Field[i][j] = 1;
+			}
+			if (i == 3 && j == 4) {
+				Game_Field[i][j] = 2;
+			}
+			if (i == 4 && j == 3) {
+				Game_Field[i][j] = 2;
+			}
+			if (i == 4 && j == 4) {
+				Game_Field[i][j] = 1;
+			}
+			else {
+				Game_Field[i][j] = 0;
+			}
+		}
+	}
 
+
+}
+
+void Update_Display() {
+	
 
 
 }
